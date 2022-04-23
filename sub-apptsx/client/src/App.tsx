59287689e72108bc25ed4b7c6eef5040a0ae7c9a
laujoseph <97426227+laujoseph@users.noreply.github.com>
2022-04-23 21:30:57 +0800

    Setting up, navbar created

---
+++ sub-apptsx/client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Nav/Nav";
+import Hero from "./components/Hero/Hero";
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
