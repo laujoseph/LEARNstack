44f2b6ea7227bd564d2d007a5d3fae8ff120bf5c
laujoseph <97426227+laujoseph@users.noreply.github.com>
2022-04-28 21:44:53 +0800

    Created protected route

--- sub-apptsx/client/src/App.tsx
+++ sub-apptsx/client/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Nav/Nav";
 import Articles from "./components/pages/Articles";
 import LandingPage from "./components/pages/LandingPage";
+import { ProtectedRoute } from "./routes/ProtectedRoute";
+
 function App() {
   return (
     <div>
@@ -9,7 +11,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/articles" element={<Articles />} />
+          <Route path="/articles" element={<ProtectedRoute />}>
+            <Route path="/articles" element={<Articles />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
