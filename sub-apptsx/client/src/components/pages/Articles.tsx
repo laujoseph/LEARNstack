1e39c184dfb2903659c48459a3e92c03f1d4ded9
laujoseph <97426227+laujoseph@users.noreply.github.com>
2022-04-29 17:36:25 +0800

    update articles page

--- sub-apptsx/client/src/components/pages/Articles.tsx
+++ sub-apptsx/client/src/components/pages/Articles.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 const Articles = () => {
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+  const fetchArticles = async () => {
+    const response = await axios.get("http://localhost:8080/articles");
+    console.log(response);
+  };
   return <div>Articles</div>;
 };
 
