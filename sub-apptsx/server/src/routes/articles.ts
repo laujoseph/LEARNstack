29465a7ea917a2ae72e15d63b00df3a22438ce97
laujoseph <97426227+laujoseph@users.noreply.github.com>
2022-05-03 12:06:31 +0800

    fixed bug on categorized views
    - will only show articles based off users subscription plan

--- sub-apptsx/server/src/routes/articles.ts
+++ sub-apptsx/server/src/routes/articles.ts
@@ -64,16 +64,54 @@ router.post("/create", checkAuth, async (req, res) => {
 
 router.get("/:category", checkAuth, async (req: any, res: any) => {
   let category = req.params.category;
-  const articles = Article.find(
-    { category: category },
-    (err: any, result: any) => {
-      if (err) {
-        res.send(err);
-      }
+  // const articles = Article.find(
+  //   { category: category },
+  //   (err: any, result: any) => {
+  //     if (err) {
+  //       res.send(err);
+  //     }
 
-      res.send(result);
+  //     res.send(result);
+  //   }
+  // );
+  const user = await User.findOne({ email: req.user });
+  // this gets all the subscriptions of this particular customer
+  const subscriptions = await stripe.subscriptions.list(
+    {
+      customer: user.stripeCustomerId,
+      status: "all",
+      expand: ["data.default_payment_method"],
+    },
+    {
+      apiKey: process.env.STRIPE_SECRET_KEY,
     }
   );
+  // do not return articles if user has no subscription plan
+  if (!subscriptions.data.length) return res.json([]);
+
+  //@ts-ignore
+  const plan = subscriptions.data[0].plan.nickname;
+
+  // conditionally render articles based on user plan
+  if (plan === "Basic") {
+    const articles = await Article.find({
+      access: "Basic",
+      category: category,
+    });
+    return res.json(articles);
+  } else if (plan === "Standard") {
+    const articles = await Article.find({
+      // get all articles that have access that are either basic or standard
+      access: { $in: ["Basic", "Standard"] },
+      category: category,
+    });
+    return res.json(articles);
+  } else {
+    const articles = await Article.find({});
+    return res.json(articles);
+  }
+
+  res.json(plan);
   // console.log(articles);
 });
 
